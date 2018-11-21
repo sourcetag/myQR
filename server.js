dda700da0a929f52657e13e102b43d767aa01f98
Andrew Lara <drewlata@gmail.com>
2018-11-21 12:46:12 -0600

    fix favicon issue

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const favicon = require('express-favicon');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
@@ -23,7 +24,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(express.favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(methodOverride("_method"));
 app.use(flash());
 
