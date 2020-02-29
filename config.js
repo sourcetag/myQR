c9be2b5c6001c922ecc7ec3d941fd2511bc71b6f
korrio <iamnotkorr@gmail.com>
2020-02-29 22:41:40 +0700

    make it better

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/myqr';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-myqr";
 exports.PORT = process.env.PORT || 8080;
-exports.SECRET = process.env.SECRET;
+exports.SECRET = process.env.SECRET || "1212312121";
