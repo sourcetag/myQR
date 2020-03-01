755fda58d2a5eb3f01c42d5b560b3ce2b79d0693
korrio <iamnotkorr@gmail.com>
2020-03-01 07:15:29 +0700

    srctrk init

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://admin:admin1234@ds263018.mlab.com:63018/qvery-manager';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost/test-myqr";
+let ENVIRONMENT = "dev";
+exports.DATABASE_URL = (ENVIRONMENT == "dev") ? "mongodb://localhost:27017/qvery-manager" : 'mongodb://admin:admin1234@ds263018.mlab.com:63018/qvery-manager';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://localhost:27017/qvery-manager";
 exports.PORT = process.env.PORT || 8080;
 exports.SECRET = process.env.SECRET || "1212312121";
